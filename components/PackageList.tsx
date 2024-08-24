f3b858880c0f2f4c1925858acf1d99d4d25a9a81
GloryWong <glorywong1001@gmail.com>
2024-08-24 11:33:32 +0800

    fix(package list): width full

--- components/PackageList.tsx
+++ components/PackageList.tsx
@@ -20,26 +20,28 @@ export default function PakcageList({ userName, onSelect }: { userName: string, 
           <div className='text-red-500'>Failed to load data {JSON.stringify(error.message)}</div> :
         isLoading ?
           <Spinner /> :
-          <ScrollShadow size={80}>
-            <Listbox aria-label="Package list" variant="bordered" onAction={onSelect} selectionMode="single">
-              { 
-                (data ?? []).map(v => (
-                  <ListboxItem key={v.name} textValue={v.name} title={
-                  <div className="flex items-center gap-2">
-                    <span className="font-bold text-lg text-wrap">{v.name}</span>
-                    <Chip variant="flat" size="sm">{v.version}</Chip>
-                  </div>}
-                  description={v.description}
-                    endContent={
-                      <Tooltip content={`Go to the package ${v.name} in npm`} delay={200}>
-                        <Link className="text-gray-500" isExternal showAnchorIcon anchorIcon={<Icon icon="mdi:launch" />} href={`https://www.npmjs.com/package/${v.name}`} />
-                      </Tooltip>
-                      }>
-                  </ListboxItem>
-                ))
-              }
-            </Listbox>
-          </ScrollShadow>
+          <div className="w-full flex-grow min-h-0">
+            <ScrollShadow size={80} className="h-full">
+              <Listbox aria-label="Package list" variant="bordered" onAction={onSelect} selectionMode="single">
+                { 
+                  (data ?? []).map(v => (
+                    <ListboxItem key={v.name} textValue={v.name} title={
+                    <div className="flex items-center gap-2">
+                      <span className="font-bold text-lg text-wrap">{v.name}</span>
+                      <Chip variant="flat" size="sm">{v.version}</Chip>
+                    </div>}
+                    description={v.description}
+                      endContent={
+                        <Tooltip content={`Go to the package ${v.name} in npm`} delay={200}>
+                          <Link className="text-gray-500" isExternal showAnchorIcon anchorIcon={<Icon icon="mdi:launch" />} href={`https://www.npmjs.com/package/${v.name}`} />
+                        </Tooltip>
+                        }>
+                    </ListboxItem>
+                  ))
+                }
+              </Listbox>
+            </ScrollShadow>
+          </div>
       }
     </div>
   );
